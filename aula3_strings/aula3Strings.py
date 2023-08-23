314db9bc8800b042bae45e59dc7302aa0f8fa368
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2023-08-23 10:33:42 -0300

    Aula 3 Explicação sobre Strings

---
+++ aula3_strings/aula3Strings.py
@@ -0,0 +1,20 @@
+# Aspas simples
+print('Leandro Oliveira')
+
+#Aspas Duplas
+print("Leandro Oliveira")
+
+# Usando aspas simples ' no começo, podemos usar aspas duplas ou vice e versa.
+print('Leandro "Oliveira"')
+print("Leandro 'Oliveira'")
+
+
+
+# Escape \
+print("Leandro \"Oliveira\"")
+
+# r usando mostra os dados do escape no print
+print(r"Leandro \"Oliveira\"")
+
+
+
