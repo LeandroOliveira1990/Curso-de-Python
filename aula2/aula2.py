50522c7c5843c85f40444eb5ef798cf22d47dbbb
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2023-08-23 10:16:54 -0300

    Aula 2, explicação sobre a função print, comandos sep e end.

---
+++ aula2/aula2.py
@@ -0,0 +1,3 @@
+print(12, 34, 1011, sep='_') # Comando sep indica qual separador você quer usar no caso está _.
+print(56, 78, sep="-", end='##') # Comando end indica o quer vai fazer no final da linha, se estiver em branco é uma quebra de linha, mas no caso coloquei duas #
+print(9, 10, sep="-")
