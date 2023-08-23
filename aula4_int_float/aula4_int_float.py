0991d28f3b4665a96a503c52fa33f310816fd105
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2023-08-23 11:29:54 -0300

    Aula 4 explicação sobre int e float e a função type

---
+++ aula4_int_float/aula4_int_float.py
@@ -0,0 +1,16 @@
+#int
+print(11)
+print(-11)
+print(0)
+
+#float
+print(1.1, 10.11)
+print(0.0, -1.5)
+
+#type mostra o tipo da váriavel em Python
+print(type('Leandro'))
+print(type(1))
+print(type(-1))
+print(type(1.1), type(-1.1), type(0.0) )
+
+
