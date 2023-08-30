ede9fab93d85cd30e07d39437795255572d7c5eb
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2023-08-30 16:38:07 -0300

    Aula 7 introdução a variaveis

---
+++ aula7_variaveis/aula7_variaveis.py
@@ -0,0 +1,13 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+
+nome_completo = 'Leandro de Oliveira Silva'
+soma_dois_mais_dois = 2 + 2 
+print(nome_completo) # Mostrar na tela o que há na variavel
+print(soma_dois_mais_dois)
+
+nome = 'Leandro'
+idade= 33
+maior_de_idade = idade >= 18
+print('Nome: ', nome)
+print('Idade: ', idade)
+print('É maior de idade? ', maior_de_idade)
