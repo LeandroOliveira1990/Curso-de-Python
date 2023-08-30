fae9d58a05321281d884e348598e6cb15d32e7e3
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2023-08-30 15:35:26 -0300

    Aula 6 conversão de tipos

---
+++ aula6_conversao_de_tipos/aula6_conversao_de_tipos.py
@@ -0,0 +1,17 @@
+# conversão de tipos, coersão.
+# É o ato de converter um tipo em outro
+
+print('1', type('1'))
+print(1, type(1))
+print(int('1'), type(int('1'))) # realizando conversão de string para inteiro
+print(int('1') + 1)
+print(11)
+print(str(11))
+print(type (str(11)))
+
+
+
+
+
+
+
