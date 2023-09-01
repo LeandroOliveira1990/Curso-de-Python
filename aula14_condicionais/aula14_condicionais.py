9c822be371f76110b758d18fc61f63cf50257ebe
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2023-09-01 16:17:12 -0300

    Aula 14 if elif else

---
+++ aula14_condicionais/aula14_condicionais.py
@@ -0,0 +1,17 @@
+# if / elif /else
+# se / se não se / se não
+
+
+resposta = input('Você quer "entrar" ou "sair"? ')
+
+if resposta == 'entrar':
+    print('Você entrou no sistema')
+
+elif resposta == 'sair':
+    print('Você saiu do sistema')
+
+else:
+    print('Opção incorreta erro!!!!')
+
+
+print('Fora dos blocos')
