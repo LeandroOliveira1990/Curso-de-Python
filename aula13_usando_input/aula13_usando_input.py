6525c7fe395a714214b483be0606ec4cd3e00c66
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2023-09-01 14:20:23 -0300

    Aula 13 usando a função input para capturar dados do usuário

---
+++ aula13_usando_input/aula13_usando_input.py
@@ -0,0 +1,6 @@
+nome = input('Qual o seu nome? ')
+print(f'O nome digitado é: {nome}')
+numero_1 = int(input('Digite um número: ')) # usando type cast para alterar de str para int direto no input
+numero_2 = int(input('Digite outro número: '))
+
+print(f'A soma dos números é: {numero_1 + numero_2}')
